38c329af9d47798c74dd47487fc162f73c72c183
Shankhadeep <103721813+skd04@users.noreply.github.com>
2024-08-18 22:19:01 +0700

    Add files via upload
    Take a look at my new website - it's my very first personal project!

---
+++ Script/script.js
@@ -0,0 +1,11 @@
+// script.js
+const navLinks = document.querySelectorAll('nav a');
+
+navLinks.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+    const sectionId = link.getAttribute('href');
+    const section = document.querySelector(sectionId);
+    section.scrollIntoView({ behavior: 'smooth' });
+  });
+});
